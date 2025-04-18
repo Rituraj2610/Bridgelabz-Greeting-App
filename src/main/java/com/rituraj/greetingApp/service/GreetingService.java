450f9b8d69bfa10a0b61906a9232ee46bb59346d
Rituraj2610 <26rituraj@gmail.com>
2025-04-18 11:39:15 +0530

    [Rituraj] ADD . UC3_UpdatingGet

--- src/main/java/com/rituraj/greetingApp/service/GreetingService.java
+++ src/main/java/com/rituraj/greetingApp/service/GreetingService.java
@@ -9,8 +9,16 @@ import org.springframework.web.bind.annotation.PutMapping;
 @Service
 public class GreetingService {
 
-    public String getGreeting() {
-        return "{\"message\": \"Hello, this is a GET request!\"}";
+    public String getGreeting(String firstName, String lastName) {
+        if (firstName != null && lastName != null) {
+            return String.format("Hello, %s %s!", firstName, lastName);
+        } else if (firstName != null) {
+            return String.format("Hello, %s!", firstName);
+        } else if (lastName != null) {
+            return String.format("Hello, %s!", lastName);
+        } else {
+            return "Hello World!";
+        }
     }
 
 
